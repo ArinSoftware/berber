1b4d3394dea3b7eb04a46491a1707b43f98f3da7
gurcancekic <arinyazilim@gmail.com>
2023-07-10 23:09:52 +0300

    tailwind css settings updated and components cleared

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl">Barber</h1>
   </main>
 </template>
