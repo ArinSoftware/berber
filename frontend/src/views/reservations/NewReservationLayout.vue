d189636a9a273ae7a258b07a26907cd81f4fbe08
gurcancekic <arinyazilim@gmail.com>
2023-07-11 23:21:05 +0300

    reservation pages tailwind classes added

--- frontend/src/views/reservations/NewReservationLayout.vue
+++ frontend/src/views/reservations/NewReservationLayout.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { useRoute } from "vue-router"
 
+const route = useRoute()
 </script>
 
 <template>
-  <h1>New Reservation Layout</h1>
+  <nav class="my-5 flex gap-3">
+    <RouterLink :to="{ name: 'new-reservation' }"
+      class="flex-1 text-center p-3 uppercase font-bold hover:bg-blue-600 hover:text-white"
+      :class="route.name === 'new-reservation' ? 'text-white bg-blue-500' : 'bg-white text-blue-500'">
+      Services
+    </RouterLink>
+    <RouterLink :to="{ name: 'reservation-details' }"
+      class="flex-1 text-center p-3 uppercase font-bold hover:bg-blue-600 hover:text-white"
+      :class="route.name === 'reservation-details' ? 'text-white bg-blue-500' : 'bg-white text-blue-500'">
+      Details
+    </RouterLink>
+  </nav>
+
   <RouterView />
 </template>
--- frontend/src/views/reservations/ReservationsLayout.vue
+++ frontend/src/views/reservations/ReservationsLayout.vue
@@ -3,8 +3,22 @@
 </script>
 
 <template>
-  <div>
-    <h1>Reservation Layout</h1>
+  <div class="flex justify-between">
+    <h1 class="text-2xl lg:text-6xl font-black text-white">Barber</h1>
+    <div class="flex flex-col space-y-5">
+      <div class="flex gap-2 items-center justify-end">
+        <p class="text-white text-right">Hi Username</p>
+        <button type="button" class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-black rounded-lg">
+          Sign Off
+        </button>
+      </div>
+      <nav class="flex gap-2 items-center justify-end">
+        <button class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg">My Reservations</button>
+        <RouterLink class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700"
+          :to="{ name: 'new-reservation' }">New
+          Reservation</RouterLink>
+      </nav>
+    </div>
   </div>
   <main>
     <RouterView />
